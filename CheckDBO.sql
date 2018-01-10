5bc0e2f7bb31b86c6b17a7f71755259768131f1f
huatuostudio <huatuostudio@gmail.com>
2018-01-10 14:43:00 +1100

    Update CheckDBO.sql

--- CheckDBO.sql
+++ CheckDBO.sql
@@ -1,5 +1,8 @@
 # My pet hate is the DBO is set not to a service account or SA but a user account that might be disabled or removed for whatever the reason
 
+Use master
+Go
+
 SELECT name,
 	suser_sname(owner_sid) as DBO,
        DATABASEPROPERTYEX(name, 'Recovery') as recoveryMode, 
